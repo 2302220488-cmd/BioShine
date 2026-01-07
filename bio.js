82ec7bbd024f97c7cfc6318ac33557165ef54787
2302220488-cmd <2302220488@students.satyaterrabhinneka.ac.id>
2026-01-07 21:59:45 +0700

    Update bio.js
    Signed-off-by: 2302220488-cmd <2302220488@students.satyaterrabhinneka.ac.id>

--- bio.js
+++ bio.js
@@ -1,16 +1,20 @@
-function kirimWA() {
+document.getElementById("orderForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
     var nama = document.getElementById("nama").value;
     var varian = document.getElementById("varian").value;
     var jumlah = document.getElementById("jumlah").value;
     var catatan = document.getElementById("catatan").value;
 
     var pesan =
-        "Halo, saya mau pesan BioShine\n\n" +
-        "Nama: " + nama + "\n" +
-        "Varian: " + varian + "\n" +
-        "Jumlah: " + jumlah + "\n" +
+        "Halo, saya mau pesan BioShine%0A%0A" +
+        "Nama: " + nama + "%0A" +
+        "Varian: " + varian + "%0A" +
+        "Jumlah: " + jumlah + "%0A" +
         "Catatan: " + catatan;
 
-    var url = "https://wa.me/6283896690924?text=" + encodeURIComponent(pesan);
+    var nomorWA = "6283896690924";
+    var url = "https://wa.me/" + nomorWA + "?text=" + pesan;
+
     window.open(url, "_blank");
-}
+});
